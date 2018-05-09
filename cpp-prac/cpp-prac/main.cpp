2879aa94ff25ffd5762cb020241cb024ff050c0a
Patrick Wu <feiyang.wu1996@gmail.com>
2018-05-09 17:18:57 +0800

    new commit

---
+++ cpp-prac/cpp-prac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-prac
+//
+//  Created by Feiyang Wu on 2018/5/9.
+//  Copyright © 2018 Feiyang Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
